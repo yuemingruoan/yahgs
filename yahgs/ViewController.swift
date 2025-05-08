c9f4d1466afcc5838d55d42c875630f8a43c0c3c
时雨 <270598250@qq.com>
2025-05-08 20:37:49 +0800

    创建项目文件，以及任务栏部分的编写

---
+++ yahgs/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  yahgs
+//
+//  Created by 时雨 on 2025/5/8.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
